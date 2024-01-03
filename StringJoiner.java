797afa50618da312c655c7fb47ef62df0f4ba426
Rasmi Ranjan <67892611+arpit-mhnty@users.noreply.github.com>
2024-01-03 23:11:35 +0530

    Update StringJoiner.java

--- StringJoiner.java
+++ StringJoiner.java
@@ -1,1 +1,29 @@
+import java.util.StringJoiner;
 
+public class linkedlist {
+	public static void main(String Args[]) {
+		StringJoiner str = new StringJoiner(",", "[", "}");
+    
+		// if stringjoiner is empty it will take by default
+		str.setEmptyValue("add this");
+		System.out.print(str);
+		str.add("null");
+    
+		StringJoiner str1 = new StringJoiner(":", "[", "]");
+		str1.add("kuchv");
+		str1.add("ker");
+    
+		// make sure to create new stringjoiner to store it
+		StringJoiner st = str.merge(str1);
+		System.out.println(st);
+    
+		// it print the length
+		System.out.println(str.length());
+		
+		
+		//output: add this[null,kuchv:ker}
+		//16
+		
+
+	}
+}
